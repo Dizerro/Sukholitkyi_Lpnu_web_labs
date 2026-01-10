2ab6d5f51c9d4bbabe07d0d78a5c6572ac348eb4
Dizerro <zakzaksukhar@gmail.com>
2026-01-10 19:51:39 +0200

    minor update

--- lab_6/travel-helper/src/App.js
+++ lab_6/travel-helper/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";              // <<--- make sure this is here
+import "./App.css";
 import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
 
